57ddd3f2993135758fac5992458412dbd16e170a
Jevon Beckles <jevonbeck@yahoo.com>
2017-06-04 19:25:04 +0100

    Minor update - Use UniMemoryComputeCoreInterface

--- app/src/main/java/org/ricts/abstractmachine/components/compute/core/UniMemoryComputeCore.java
+++ app/src/main/java/org/ricts/abstractmachine/components/compute/core/UniMemoryComputeCore.java
@@ -3,18 +3,20 @@ package org.ricts.abstractmachine.components.compute.core;
 import android.content.res.Resources;
 
 import org.ricts.abstractmachine.components.interfaces.MemoryPort;
+import org.ricts.abstractmachine.components.interfaces.UniMemoryComputeCoreInterface;
 
 /**
  * Created by Jevon on 10/03/2017.
  */
 
-public abstract class UniMemoryComputeCore extends ComputeCore {
+public abstract class UniMemoryComputeCore extends ComputeCore implements UniMemoryComputeCoreInterface {
     protected MemoryPort dataMemory;
 
     public UniMemoryComputeCore(Resources res) {
         super(res);
     }
 
+    @Override
     public void setDataMemory(MemoryPort memory) {
         dataMemory = memory;
     }
